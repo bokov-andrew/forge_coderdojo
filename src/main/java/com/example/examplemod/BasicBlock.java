36fc9cbff422dc46d4f2cb3e39075d5a34388e7b
A Bokov <lively.bokov@gmail.com>
2020-10-03 10:56:56 -0500

    Initial commit of Andrew's files

--- src/main/java/com/example/examplemod/BasicBlock.java
+++ src/main/java/com/example/examplemod/BasicBlock.java
@@ -3,8 +3,9 @@ package com.example.examplemod;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
-public class BasicBlock extends Block {
+public class BasicBlock extends Block{
 	public BasicBlock () {
-		super(Block.Properties.create(Material.ROCK));
-	}
+			super(Block.Properties.create(Material.ROCK));
+		}
+
 }
